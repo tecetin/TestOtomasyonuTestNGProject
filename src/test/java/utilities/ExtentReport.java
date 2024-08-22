adf6b414b20690271c9ce2c3c5a5a89b65d46218
tecetin <tugba.uluses@gmail.com>
2024-08-22 12:32:44 +0300

    US_002 tamamlandı.

--- src/test/java/utilities/ExtentReport.java
+++ src/test/java/utilities/ExtentReport.java
@@ -2,57 +2,66 @@ package utilities;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.beust.jcommander.FuzzyMap;
+import org.testng.IResultMap;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
+import javax.naming.Name;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public abstract class ExtentReport {
-    protected static ExtentReports extentReports;
+    protected static ExtentReports extent;
     protected static ExtentTest extentTest;
     protected static ExtentHtmlReporter extentHtmlReporter;
+
     @BeforeTest(alwaysRun = true)
     public void setUpTest() {
-        extentReports = new ExtentReports();
-        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
-        String dosyaYolu = "target/extentReport/report" + tarih + ".html";
+        extent = new ExtentReports(); // Raporlamayi baslatir
+
+        //rapor oluştuktan sonra raporunuz nereye eklensin
+        String tarih = new SimpleDateFormat("_dd_MM_yy-HH_mm_ss").format(new Date());
+        String dosyaYolu = System.getProperty("user.dir") + "/target/extentReport/Report" + tarih + ".html";
+
+        //oluşturmak istediğimiz raporu (html formatında) başlatıyoruz, dosya yolunu belirliyoruz.
         extentHtmlReporter = new ExtentHtmlReporter(dosyaYolu);
-        extentReports.attachReporter(extentHtmlReporter);
-        extentReports.setSystemInfo("Browser", ConfigReader.getProperty("browser"));
-        extentReports.setSystemInfo("Tester", ConfigReader.getProperty("testerName"));
+        extent.attachReporter(extentHtmlReporter);
+
+        // Bilgileri buraya ekle
+        extent.setSystemInfo("Browser", ConfigReader.getProperty("browser"));
+        extent.setSystemInfo("Tester", ConfigReader.getProperty("testerName"));
         extentHtmlReporter.config().setDocumentTitle("Extent Report");
         extentHtmlReporter.config().setReportName("TestNG Reports");
+        extentHtmlReporter.config().setTheme(Theme.DARK);
     }
+
     @AfterMethod(alwaysRun = true)
     public void tearDownMethod(ITestResult result) throws IOException {
         if (result.getStatus() == ITestResult.FAILURE) { // eğer testin sonucu başarısızsa
-            String screenshotLocation = ReusableMethods.tumSayfaResmi(result.getName());
-            extentTest.fail(result.getName());
-            extentTest.addScreenCaptureFromPath(screenshotLocation);
-            extentTest.fail(result.getThrowable());
-        } else if (result.getStatus() == ITestResult.SKIP) { // eğer test çalıştırılmadan geçilmezse
-            extentTest.skip("Test Case is skipped: " + result.getName());
+            String screenshotLocation = ReusableMethods.sayfaSSBase64();
+            extentTest.fail(result.getName() + " testi başarısız oldu.");
+            extentTest.fail(result.getThrowable().getMessage(),
+                    MediaEntityBuilder.createScreenCaptureFromBase64String((screenshotLocation)).build());
+        } else if (result.getStatus() == ITestResult.SUCCESS) { // Eğer test başarılı olduysa
+            extentTest.pass("Test Case başarılı: " + result.getName());
         }
-        Driver.closeDriver();
     }
 
     @AfterTest(alwaysRun = true)
     public void tearDownTest() {
-        extentReports.flush();
-    }
-    public static String ekranResmi(WebDriver driver) {
-        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
-    }
-    public static String WEResmi(WebElement element) {
-        return element.getScreenshotAs(OutputType.BASE64);
+
+        // Raporu bitirir ve dosyaya kaydeder
+        if (extent != null) {
+            extent.flush();
+        }
     }
 }
